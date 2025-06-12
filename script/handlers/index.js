824f26146953458902289dfe2fba5074d4d2b19f
kristianusnata <kristianusnata@gmail.com>
2025-06-12 15:16:24 +0700

    updated js

--- script/handlers/index.js
+++ script/handlers/index.js
@@ -1,5 +1,4 @@
-import { carousel } from "./carousel.js";
 import { error } from "./error.js";
 import { validation } from "./validation.js";
 
-export { carousel, error, validation };
+export { error, validation };
